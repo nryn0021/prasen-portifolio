11e78343a0a61c80dbe7c6b949d03d28438d73bd
nryn0021 <80402194+nryn0021@users.noreply.github.com>
2024-02-24 10:32:02 +0530

    Update Logo.tsx

--- prasen-portifolio/src/components/Logo.tsx
+++ prasen-portifolio/src/components/Logo.tsx
@@ -11,24 +11,6 @@ function Logo() {
             d="M 10 0 L 10 30 L 20 30 Q 30 30 30 20 Q 30 10 20 10 L 10 10 Z"
           />
         </g>
-        <g id="N" transform="translate(50.000000, 25.000000)">
-          <path
-            fill="currentColor"
-            d="M 10 0 L 10 30 L 20 30 Q 40 15 20 0 Z"
-          />
-        </g>
-        <path
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M 50, 5
-             L 11, 27
-             L 11, 72
-             L 50, 95
-             L 89, 73
-             L 89, 28 z"
-        />
       </g>
     </svg>
   );
